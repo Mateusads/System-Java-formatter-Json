eb9b611c79bc060fe09052641c57a8d24112ddc0
mateus <mateusowmedeiros@gmail.com>
2022-03-10 00:38:38 -0300

    Criando o projeto e suas entidades

---
+++ src/main/java/br/com/formatter/model/Order.java
@@ -0,0 +1,10 @@
+package br.com.formatter.model;
+
+import java.time.LocalDate;
+
+public class Order {
+
+    private int id;
+    private double total;
+    private LocalDate date;
+}
---
+++ src/main/java/br/com/formatter/model/Product.java
@@ -0,0 +1,7 @@
+package br.com.formatter.model;
+
+public class Product {
+
+    private int id;
+    private double value;
+}
