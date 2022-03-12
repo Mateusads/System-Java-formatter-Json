97f63c86e7f5f2c1d31750880d495bfc4bb60691
mateus <mateusowmedeiros@gmail.com>
2022-03-12 01:12:34 -0300

    Melhorando nome do metodo reading zero

--- src/main/java/br/com/json/formatter/service/ProcessElementsFile.java
+++ src/main/java/br/com/json/formatter/service/ProcessElementsFile.java
@@ -5,12 +5,12 @@ import java.util.Map;
 public class ProcessElementsFile {
 
     public void processingElements(Map<String, String> elementsMap){
-        elementsMap.put("user_id", removingZeros(elementsMap.get("user_id")));
-        elementsMap.put("order_id", removingZeros(elementsMap.get("order_id")));
-        elementsMap.put("product_id", removingZeros(elementsMap.get("product_id")));
+        elementsMap.put("user_id", removingLeadingZeros(elementsMap.get("user_id")));
+        elementsMap.put("order_id", removingLeadingZeros(elementsMap.get("order_id")));
+        elementsMap.put("product_id", removingLeadingZeros(elementsMap.get("product_id")));
     }
 
-    private String removingZeros(String element) {
+    private String removingLeadingZeros(String element) {
         int index = 0;
         for(int i = 0; i < element.length(); i++){
             var itemElement = element.charAt(i);
