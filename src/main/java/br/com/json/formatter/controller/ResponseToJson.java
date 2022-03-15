0045335b8da92fadec84fc0967439c27eb65f592
mateus <mateusowmedeiros@gmail.com>
2022-03-14 21:31:49 -0300

    Melhorando o recebimento da path com exceptions e formatando o tipo LocalDate com Gson.

--- src/main/java/br/com/json/formatter/controller/ResponseToJson.java
+++ src/main/java/br/com/json/formatter/controller/ResponseToJson.java
@@ -2,9 +2,11 @@ package br.com.json.formatter.controller;
 
 import br.com.json.formatter.model.User;
 import br.com.json.formatter.service.ReadingFileService;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Set;
 
 public class ResponseToJson {
@@ -15,12 +17,17 @@ public class ResponseToJson {
     }
     private String createDataForJson(Set<User> usersCreated){
         Gson userJson = new GsonBuilder()
-                .setDateFormat("yyyy-MM-dd")
+                .setPrettyPrinting()
+                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
                 .create();
         StringBuilder userResponse = new StringBuilder();
-        for(var user : usersCreated){
-            userResponse.append(userJson.toJson(user));
-        }
+        userResponse.append(userJson.toJson(usersCreated));
         return userResponse.toString();
+    }
+    private class LocalDateAdapter implements JsonSerializer<LocalDate> {
+
+        public JsonElement serialize(LocalDate date, Type typeOfSrc, JsonSerializationContext context) {
+            return new JsonPrimitive(date.format(DateTimeFormatter.ISO_LOCAL_DATE)); // "yyyy-mm-dd"
+        }
     }
 }
--- src/main/java/br/com/json/formatter/main/Main.java
+++ src/main/java/br/com/json/formatter/main/Main.java
@@ -5,13 +5,19 @@ import br.com.json.formatter.controller.RequestFileConverter;
 public class Main {
 
     public static void main(String[] args) {
-        String path = "./src/fileDataTest.txt";
-        if(args != null && args.length > 0) {
-            path = args[0];
-        }
-        RequestFileConverter requestFileConverter = new RequestFileConverter();
-        var jsonTheFile= requestFileConverter.readingFile(path);
-        System.out.println(jsonTheFile);
 
+        try {
+            var path = args[0];
+            RequestFileConverter requestFileConverter = new RequestFileConverter();
+            var jsonTheFile = requestFileConverter.readingFile(path);
+            System.out.println(jsonTheFile);
+        }catch (NullPointerException e ){
+            System.out.println("Error: " + e.getMessage());
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Path not found");
+            System.out.println("Error: " + e.getMessage());
+        }finally {
+            System.out.println("Program closed");
+        }
     }
 }
